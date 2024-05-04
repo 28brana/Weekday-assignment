2c42245bdde044adb910c10b3942101dd361d427
Bharat <28brana@gmail.com>
2024-05-04 17:37:43 +0530

    JD Card Designed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import useFetch from "./hook/useFetch";
+import Main from "./Main";
 
 function App() {
   const { loading, error, data, totalCount, fetchData } = useFetch();
   return (
     <div className="App">
       <button onClick={fetchData}>Click</button>
+      <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import Card from "./component/card";
+
+const Main=({data})=>{
+    return (
+        <div>
+            <Card/>
+        </div>
+    )
+}
+
+export default Main;
